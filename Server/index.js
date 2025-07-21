925106d3571d7159add6a594aeb0ed77a3f30f8b
asiduki <147263246+asiduki@users.noreply.github.com>
2025-07-21 17:21:50 +0530

    work

--- Server/index.js
+++ Server/index.js
@@ -10,10 +10,11 @@ app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
 
+const Port = process.env.PORT || 5000;
 app.use("/Login",LoginRouter)
 
 
 
-app.listen(5000 ,()=>{
-    console.log("server is running on 5000")
+app.listen(Port ,()=>{
+    console.log(`server is running on ${Port}`)
 })

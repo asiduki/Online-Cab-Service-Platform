a2ff0861cebd43a6ba0c42c007cf0d0aae3563d4
asiduki <147263246+asiduki@users.noreply.github.com>
2025-07-21 17:19:36 +0530

    work on data bases

--- Server/config/connection.js
+++ Server/config/connection.js
@@ -1,6 +1,9 @@
 const {mongoose} = require("mongoose");
+require('dotenv').config();
 const dbgr = require("debug")("development:mongoose");
 const config = require("config");
+
+const URL = process.env.URL;
 
 mongoose.connect(config.get("URL")).then(()=>{
     dbgr("connected");
